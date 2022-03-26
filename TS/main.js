5413d4c75674a51e74aec16d94d8ee5739bb1554
edwinespinosad <edwinlbr240@gmail.com>
2022-03-26 15:52:14 -0600

    Angular example

---
+++ TS/main.js
@@ -0,0 +1,10 @@
+// alert("Mensaje Edwin gggghg y Esteban aaaaaaaaaa");
+var cadenaN = 1234;
+var variable = "Texto";
+function getNumero(numero) {
+    if (numero === void 0) { numero = 12; }
+    return "El numero es: " + numero;
+}
+console.log(getNumero(100));
+console.log(cadenaN);
+console.log(variable);
--- TS/main.ts
+++ TS/main.ts
@@ -1,1 +1,16 @@
-alert("Mensaje");
+// alert("Mensaje Edwin gggghg y Esteban aaaaaaaaaa");
+
+let cadenaN:string|number = 1234;
+
+type alfanumerico = string|number;
+
+let variable:alfanumerico = "Texto";
+
+function getNumero(numero:number = 12):string{
+    return "El numero es: "+numero;
+}
+
+console.log(getNumero(100));
+
+console.log(cadenaN);
+console.log(variable);
